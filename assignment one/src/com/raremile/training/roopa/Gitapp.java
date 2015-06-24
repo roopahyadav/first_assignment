96a3efbaf9795ac698daf9ef2a6a543541f04e07
roopahyadav <roopa.h@raremile.com>
2015-06-24 15:44:04 -0700

    eclispe file is added

---
+++ assignment one/src/com/raremile/training/roopa/Gitapp.java
@@ -0,0 +1,14 @@
+package com.raremile.training.roopa;
+
+public class Gitapp {
+	public void name(String name){
+		System.out.println("My name is "+name+ " i'm learning git ");
+	}
+	public static void main(String [] args){
+		Gitapp gitapp =new Gitapp();
+		gitapp.name("Roopa");
+		
+		
+	}
+
+}
